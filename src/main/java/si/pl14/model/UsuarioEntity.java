63ba076c389efdf8e391cd2ac2aa2226dfdcc463
Xen <aron.laika@gmail.com>
2026-02-11 23:57:39 +0100

    Configuración inicial para empezar a trabajar mediante la plantilla

---
+++ src/main/java/si/pl14/model/UsuarioEntity.java
@@ -0,0 +1,5 @@
+package si.pl14.model;
+
+public class UsuarioEntity {
+
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,5 @@
 -- Aquí irán las tablas para la base de datos
+
+-- Este es el ejemplo de inicio de la plantilla (ver como referencia)
+drop table Carreras;
+create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
