7fc69437e72706eca47fb0a8efe0b1a34297dd3e
emma <emma@localhost>
2026-02-20 13:40:53 +0100

    creación de horario entity (lo he metido en el paquete de actividades pero quizá habría que ponerlo en otro)

---
+++ src/main/java/si/pl14/actividades/HorarioEntity.java
@@ -0,0 +1,41 @@
+package si.pl14.actividades;
+
+public class HorarioEntity {
+	private int idHorario;
+	private String diaSemana;
+	private String horaInicio;
+	private String horaFin;
+
+	public int getIdHorario() {
+		return idHorario;
+	}
+
+	public void setIdHorario(int idHorario) {
+		this.idHorario = idHorario;
+	}
+
+	public String getDiaSemana() {
+		return diaSemana;
+	}
+
+	public void setDiaSemana(String diaSemana) {
+		this.diaSemana = diaSemana;
+	}
+
+	public String getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(String horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public String getHoraFin() {
+		return horaFin;
+	}
+
+	public void setHoraFin(String horaFin) {
+		this.horaFin = horaFin;
+	}
+
+}
